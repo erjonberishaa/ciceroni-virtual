818a1189bdce6e17006e75ffa7d3e6cdaa911838
erjonberishaa <eb55980@ubt-uni.net>
2023-03-12 18:57:13 +0100

    Krijimi i Admin Page dhe stilizimi i saj

---
+++ adminpage.php
@@ -0,0 +1,32 @@
+<?php 
+   @include 'config.php';
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to the Admin Panel Dashboard! </title>
+	<link rel="stylesheet" href="style_dashboard.css">
+</head>
+<body>
+	<header>
+		<h1>Admin Dashboard</h1>
+	</header>
+
+	<nav >
+		<ul>
+            <li><a href="#">Home</a></li>
+        	<li><a href="#">Dashboard</a></li>
+			<li><a href="#">Users</a></li>
+			<li><a href="#">Logout</a></li>
+		</ul>
+	</nav>
+
+	<main>
+		<section>
+			<h2>Dashboard</h2>
+			<p>Welcome to the admin panel dashboard!</p>
+		</section>
+	</main>
+</body>
+</html>
--- includes/config.php
+++ includes/config.php
@@ -1,15 +1,9 @@
 <?php
 
-<<<<<<< HEAD
     $host = 'localhost';
     $username = "root";
     $password = "";
     $database = "user_db";
 
     $conn = mysqli_connect("$host", "$username", "$password", "$database");
-=======
-
-$conn = mysqli_connect('localhost','root','','user_db');
->>>>>>> 07dd5b8db9ae5160c013c1638af2c03e6358935a
-
 ?>
