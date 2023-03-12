f425e3c596ccd28adb0d12383676958696a7be8a
BrahimSylejmani <bimisylejmani56@gmail.com>
2023-03-12 23:06:36 +0100

    Login fix

--- login_form.php
+++ login_form.php
@@ -28,7 +28,7 @@ if(isset($_POST['submit'])){
       }elseif($row['user_type'] == 'user'){
 
          $_SESSION['user_name'] = $row['name'];
-         header('location:index.php');
+         header('location:oferta.php');
 
       }
      
