bc1e329a6abb5fa0b58d48e972074766c03054cf
BrahimSylejmani <bimisylejmani56@gmail.com>
2023-03-12 22:51:27 +0100

    Shtimi i ofertave

---
+++ oferta.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+    <title>Turet</title>
+</head>
+
+<body>
+
+    <?php
+        include('includes/header.php');
+    ?>
+
+    <!-- Fillimi i tureve -->
+    <section class="packages" id="turet">
+
+        <h1 class="heading">
+            <span>O</span>
+            <span>F</span>
+            <span>E</span>
+            <span>R</span>
+            <span>T</span>
+            <span>A</span>
+        </h1>
+
+        <div class="blogs-section">
+            <div class="blog-card">
+                <img src="img/berat.png" class="blog-image" alt="">
+                <h1 class="blog-title">Berat</h1>
+                <p class="blog-overview"> Vendet te cilat mund ti vizitoni ne qytetin e Beratit me kete ofert:
+                    Kalaja e Beratit, Muzeu i Onurfit, Muzeu Etnografikk, Mangalemi dhe Gorica. </p>
+                <p class="btn dark">$200</p>
+            </div>
+            <div class="blog-card">
+                <img src="img/prizren.jpg" class="blog-image" alt="">
+                <h1 class="blog-title">Prizren</h1>
+                <p class="blog-overview">Vendet te cilat mund ti vizitoni ne qytetin e Prizerenit me kete ofert:
+                    Muzeu dhe Kompleksi i Lidhjes Shqiptare të Prizrenit, Kalaja e Prizerenti, Lumbardhi dhe Kinemate e
+                    Dokufestit.</p>
+                <p class="btn dark">$300</p>
+            </div>
+            <div class="blog-card">
+                <img src="img/shkodra-final.jpg" class="blog-image" alt="">
+                <h1 class="blog-title">Shkoder</h1>
+                <p class="blog-overview">Vendet te cilat mund ti vizitoni ne qytetin e Shkodres me kete ofert:
+                    Muzeu Historik, Kalaja e Rozafes, Rrenojat ilire te Gajtanit dhe Xhamia e plumbit.</p>
+                <p class="btn dark">$150</p>
+            </div>
+            <div class="blog-card">
+                <img src="img/ksamil.jpg" class="blog-image" alt="">
+                <h1 class="blog-title">Ksamil</h1>
+                <p class="blog-overview">Plazhet te cilat mund ti vizitoni ne qytetin e Ksamilit me kete ofert:
+                    Plazhi i Ksamilit, Plazhet perball ishujve, Plazhi pasqyrat dhe Plazhi Pulebardha.</p>
+                <p class="btn dark">$400</p>
+            </div>
+        </div>
+
+    </section>
+    <!-- Fundi i tureve -->
+
+    <?php
+        include('includes/footer.php');
+    ?>
+
+
+</body>
+
+</html>
